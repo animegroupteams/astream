0a53dd0ddc811ea3d3c7f1fb7b890b532c6ac79d
animegroupteams <148884074+animegroupteams@users.noreply.github.com>
2024-08-21 09:26:09 +0100

    Update getanimelist.py

--- search/data/getanimelist.py
+++ search/data/getanimelist.py
@@ -3,5 +3,46 @@
 # running this code .
 # when you are done running this code in Google Collab create a 
 # new cell and execute this code 
+
 # from google.colab import files
-files.download('anime.json')
+# files.download('anime.json')
+# the end num can be changed to whatever number 
+#  you want to use 
+
+import requests
+import json
+
+url_template = "https://animetize-api-orcin.vercel.app/anime-list?page={}"
+start_num = 1
+end_num = 91
+animelist = []
+
+for num in range(start_num, end_num + 1):
+    url = url_template.format(num)
+    print("Getting data for page", num)
+    response = requests.get(url)
+    data = response.json()
+    
+    # Check if the 'results' key exists in the response data
+    if 'results' in data:
+        for item in data['results']:
+            # Map the new structure to the desired format
+            anime_entry = {
+                "name": item.get("title"),
+                "url": item.get("id")
+            }
+            animelist.append(anime_entry)
+    else:
+        print(f"No 'results' key in the response for page {num}")
+
+filename = 'anime.json'
+with open(filename, 'w') as f:
+    json.dump(animelist, f, indent=2)  # Added indent for better readability
+
+with open(filename, 'r') as f:
+    count = json.load(f)
+
+num_objects = len(count)
+
+print("Done! Saved as", filename)
+print("Total Anime:", num_objects)
